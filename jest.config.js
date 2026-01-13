71536089210c99ea9169d042d75726f54f6c542d
BartlomiejPop <bartlomiejpoprawa@hmail.com>
2026-01-13 20:48:00 +0100

    package.json init

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'react-native',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
